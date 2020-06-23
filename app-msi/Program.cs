be737a8af3ef94e49024271976df01851dabb77e
[vinayak] <[vinayak.korti@gmail.com]>
2020-06-23 13:10:50 +0530

    initial commit

---
+++ app-msi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app_msi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
